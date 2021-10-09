dfaa1520ea442d345fdad76fcaf53088bdb1e0e1
Viktoriia Huk <relax16011991@gmail.com>
2021-10-09 21:36:36 +0300

    + решение задачи 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+console.log(ingredientsEl);
+[...ingredients].forEach((ingredient) => {
+  const items = document.createElement("li");
+  items.textContent = ingredient;
+  ingredientsEl.appendChild(items);
+});
